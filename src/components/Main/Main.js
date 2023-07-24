02461e7d7cc94984dd7d366d268d7f3007ce0bc9
Aleksandr Viznyi <senak98@yandex.ru>
2023-07-24 21:54:11 +0400

    add music

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -5,7 +5,15 @@ function Main({ isGameStarted, buttonsToGame, countRight, countError }) {
   return (
     <main className="main">
       <h1>Тренажёр скоростной печати</h1>
-      {isGameStarted && <p>Вводите на клавиатуре буквы начиная с крайней левой</p>}
+      <div>
+        <audio controls>
+          <source src="https://575506.selcdn.ru/Методические%20Материалы/Аудио.%20Панель%204,%20Алгебра%209%20класс.mp3" type="audio/mpeg" />
+        </audio>
+      </div>
+      
+      {isGameStarted && (
+        <p>Вводите на клавиатуре буквы начиная с крайней левой</p>
+      )}
       {isGameStarted && <p>Правильно: {countRight}</p>}
       {isGameStarted && <p>Ошибки: {countError}</p>}
       {!isGameStarted && <p>Нажмите Enter, чтобы начать</p>}
@@ -20,7 +28,13 @@ function Main({ isGameStarted, buttonsToGame, countRight, countError }) {
             })
           : ""}
       </div>
+      {/* <script>
+        document.querySelector('audio').addEventListener("play", () => {
+
+        })
+      </script> */}
     </main>
+    
   );
 }
 
